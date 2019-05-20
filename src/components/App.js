bd57484aad2ce23ec1b99f0bc7c08daefd22e30c
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-05-20 14:02:14 +0300

    initial project

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        Grampus
-    </div>
-  );
+  return <div className="App">Grampus</div>;
 }
 
 export default App;
