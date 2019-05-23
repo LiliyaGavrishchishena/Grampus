ca41821cf4c40abc8fefed250b3bd25378f2fc14
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-05-23 10:47:29 +0300

    Protected routes added to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React, { Component, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 // configs
 import routes from '../configs/routes';
 import navItems from '../configs/main-nav';
+import authOperations from '../redux/auth/authOperations';
 // components
 import AppNav from './AppHeader/AppNav/AppNav';
 import MainPage from '../pages/MainPage';
 import Profile from './Profile/Profile';
+import Rating from './Rating/Rating';
+import CompanyState from './CompanyState/CompanyState';
+import YourTasks from './YourTasks/YourTasks';
+import HelpTasks from './HelpTasks/HelpTasks';
+import SmartCalendar from './SmartCalendar/SmartCalendar';
+import Settings from './Settings/Settings';
+import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
+
 // styles
 import './App.css';
 
@@ -26,11 +36,15 @@ const AsyncAboutUsPage = lazy(() =>
 class App extends Component {
   state = {};
 
+  componentDidMount() {
+    const { getUser } = this.props;
+    getUser();
+  }
+
   render() {
     return (
       <div>
         <AppNav items={navItems} />
-        <Profile />
         <Suspense fallback={MainPage}>
           <Switch>
             <Route exact path={routes.MAIN} component={MainPage} />
@@ -38,6 +52,48 @@ class App extends Component {
             <Route exact path={routes.FEATURES} component={AsyncFeaturesPage} />
             <Route exact path={routes.PRISING} component={AsyncPrisingPage} />
             <Route exact path={routes.ABOUT} component={AsyncAboutUsPage} />
+            <ProtectedRoute
+              exact
+              path={routes.PROFILE}
+              component={Profile}
+              redirectTo="/signin"
+            />
+            <ProtectedRoute
+              exact
+              path={routes.RATING}
+              component={Rating}
+              redirectTo="/signin"
+            />
+            <ProtectedRoute
+              exact
+              path={routes.STATE}
+              component={CompanyState}
+              redirectTo="/signin"
+            />
+            <ProtectedRoute
+              exact
+              path={routes.TASKS}
+              component={YourTasks}
+              redirectTo="/signin"
+            />
+            <ProtectedRoute
+              exact
+              path={routes.HELP}
+              component={HelpTasks}
+              redirectTo="/signin"
+            />
+            <ProtectedRoute
+              exact
+              path={routes.CALENDAR}
+              component={SmartCalendar}
+              redirectTo="/signin"
+            />
+            <ProtectedRoute
+              exact
+              path={routes.SETTINGS}
+              component={Settings}
+              redirectTo="/signin"
+            />
             <Redirect to="/" />
           </Switch>
         </Suspense>
@@ -46,4 +102,11 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProp = {
+  getUser: authOperations.getCurrentUser,
+};
+
+export default connect(
+  null,
+  mapDispatchToProp,
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { store, persistor } from './store/store';
+// components
 import App from './components/App';
+import Spiner from './components/Spiner/Spiner';
+// styles
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Route component={App} />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate loading={<Spiner />} persistor={persistor}>
+      <BrowserRouter>
+        <Route component={App} />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   document.getElementById('root'),
 );
