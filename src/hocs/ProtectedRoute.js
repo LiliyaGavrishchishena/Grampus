0ffce9916d3550e969bf4fc7e41f1630bf05831b
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-05-24 11:26:16 +0300

    Protected Route moved to hocs folder

--- src/hocs/ProtectedRoute.js
+++ src/hocs/ProtectedRoute.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import routes from '../../configs/routes';
-import authSelectors from '../../redux/auth/authSelectors';
+import routes from '../configs/routes';
+import authSelectors from '../redux/auth/authSelectors';
 
 const ProtectedRoute = ({
   component: Component,
@@ -28,8 +28,8 @@ const ProtectedRoute = ({
   />
 );
 
-const mstp = state => ({
+const mapStateToProps = state => ({
   isAuthenticated: authSelectors.getIsAuthenticated(state),
 });
 
-export default connect(mstp)(ProtectedRoute);
+export default connect(mapStateToProps)(ProtectedRoute);
