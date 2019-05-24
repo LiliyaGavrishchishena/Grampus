d8d54830ad2e354c1c1fe313f61b70118410de9e
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-05-24 15:59:40 +0300

    react-chartkick

---
+++ src/components/CompanyState/CompanyState.js
@@ -0,0 +1,11 @@
+// import ReactChartkick, { LineChart, PieChart } from 'react-chartkick'
+// import Highcharts from 'highcharts'
+
+// ReactChartkick.addAdapter(Highcharts)
+// class App extends Component {
+//     state = {};
+
+//     render() {
+//         return (<div>ddd</div>)
+//     }
+// }
