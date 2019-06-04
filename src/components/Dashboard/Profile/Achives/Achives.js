88db38d7a5013d8e83949c3275d0d1e20796aed4
awsy <bass_master89@mail.ru>
2019-06-04 15:33:00 +0300

    achivka file changed

--- src/components/Dashboard/Profile/Achives/Achives.js
+++ src/components/Dashboard/Profile/Achives/Achives.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import styles from './Achives.module.css';
-import MainRow from './MainRow';
 
 const Achives = () => {
   return (
     <div className={styles.achives}>
       <h3 className={styles.title}>Achievements</h3>
-      <MainRow />
+      <div>Achieves</div>
     </div>
   );
 };
